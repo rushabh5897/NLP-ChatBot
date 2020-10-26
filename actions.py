927035827742dac6a9ca123dd1b47978c719157c
Rushabh <rushabhpsancheti@gmail.com>
2020-10-26 20:36:36 +0530

    email changes

--- actions.py
+++ actions.py
@@ -30,24 +30,28 @@ class ActionSearchRestaurants(Action):
         ct = d1["location_suggestions"][0]["city_name"]
         cuisines_dict = {'bakery': 5, 'chinese': 25, 'cafe': 30, 'italian': 55, 'biryani': 7, 'north indian': 50,
                          'south indian': 85, 'thai': 95, 'mexican': 73}
-        dispatcher.utter_message("--ct--" + ct)
-        if self.checkCityAvailable(ct):
+        #dispatcher.utter_message("--ct--" + ct)
+        ct=ct.lower()
+        if not self.checkCityAvailable(ct):
             response="We do not opearate in this city yet"
+            dispatcher.utter_message("--------------------**--------------------\n" + response + "--------------------**--------------------\n")
+            return [SlotSet('flag_response', False)]
         else:
             results = zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
             d = json.loads(results)
             response = ""
             if d['results_found'] == 0:
-                response = "no results"
-                dispatcher.utter_message("--no response--")
+                response = "Extremely Sorry !!! I'm not able to find any related results to your query \n"
+                dispatcher.utter_message("--------------------**--------------------\n" + response + "--------------------**--------------------\n")
+                return [SlotSet('flag_response', False)]
             else:
                 for restaurant in d['restaurants']:
                     response = response + "Found " + restaurant['restaurant']['name'] + " in " + \
                                restaurant['restaurant']['location']['address'] + " has been rated " + \
                                restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n"
 
-        dispatcher.utter_message("-----" + response)
-        return [SlotSet('location', loc)]
+                dispatcher.utter_message("--------------------**--------------------\n" + response + "--------------------**--------------------\n")
+                return [SlotSet('flag_response', True)]
 
     def checkCityAvailable(self, ct):
         tier1_tier2_city_list=["ahmedabad", "bengaluru", "chennai", "delhi ncr", "hyderabad", "kolkata", "mumbai", "pune", "agra", "ajmer",  "aligarh", "amravati", "amritsar", "asansol", "aurangabad", "bareilly", "belgaum", "bhavnagar", "bhiwandi",  "bhopal", "bhubaneswar", "bikaner", "bilaspur", "bokaro steel city", "chandigarh", "coimbatore", "cuttack",  "dehradun", "dhanbad", "bhilai", "durgapur", "dindigul", "erode", "faridabad", "firozabad", "ghaziabad",  "gorakhpur", "gulbarga", "guntur", "gwalior", "gurgaon", "guwahati", "hamirpur", "hubli–dharwad", "indore",  "jabalpur", "jaipur", "jalandhar", "jammu", "jamnagar", "jamshedpur", "jhansi", "jodhpur", "kakinada",  "kannur", "kanpur", "karnal", "kochi", "kolhapur", "kollam", "kozhikode", "kurnool", "ludhiana", "lucknow",  "madurai", "malappuram", "mathura", "mangalore", "meerut", "moradabad", "mysore", "nagpur", "nanded", "nashik",  "nellore", "noida", "patna", "pondicherry", "purulia", "prayagraj", "raipur", "rajkot", "rajahmundry",  "ranchi", "rourkela", "salem", "sangli", "shimla", "siliguri", "solapur", "srinagar", "surat", "thanjavur",  "thiruvananthapuram", "thrissur", "tiruchirappalli", "tirunelveli", "ujjain", "bijapur", "vadodara",  "varanasi", "vasai-virar city", "vijayawada", "visakhapatnam", "vellore", "warangal"]
@@ -73,16 +77,18 @@ class ActionSendMail(Action):
         lon = d1["location_suggestions"][0]["longitude"]
         cuisines_dict = {'bakery': 5, 'chinese': 25, 'cafe': 30, 'italian': 55, 'biryani': 7, 'north indian': 50,
                          'south indian': 85, 'thai': 95, 'mexican': 73}
-        results = zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
+        results = zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 10)
         d = json.loads(results)
         response = ""
         if d['results_found'] == 0:
-            response = "no results"
+            response = "Extremely Sorry !!! I'm not able to find any related results to your query \n"
+            dispatcher.utter_message( "--------------------**--------------------\n" + response + "--------------------**--------------------\n")
+
         else:
             for restaurant in d['restaurants']:
                 response = response + "Found " + restaurant['restaurant']['name'] + " in " + \
                            restaurant['restaurant']['location']['address'] + " has been rated " + \
-                           restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n"
+                           restaurant['restaurant']['user_rating']['aggregate_rating'] + restaurant['restaurant']['average_cost_for_two'] +"\n"
 
             sender_email = "chatbotatyourhelp@gmail.com"
             receiver_email = tracker.get_slot('user_email_id')
@@ -97,7 +103,7 @@ class ActionSendMail(Action):
             # Create the plain-text and HTML version of your message
             text = """\
                         Hi,
-                        How are you?"""
+                        Hope you are doing well"""
             html = """\
                         <html>
                           <body>
@@ -119,12 +125,15 @@ class ActionSendMail(Action):
 
             # Create secure connection with server and send email
             context = ssl.create_default_context()
-            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-                server.login(sender_email, password)
-                server.sendmail(
-                    sender_email, receiver_email, message.as_string()
-                )
-            dispatcher.utter_message("Mail Sent Successfully")
+            try:
+                with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+                    server.login(sender_email, password)
+                    server.sendmail(
+                        sender_email, receiver_email, message.as_string()
+                    )
+                dispatcher.utter_message("Mail Sent Successfully")
+            except smtplib.SMTPException as e:
+                dispatcher.utter_message("You have provided incorrect email ID")
 
-        dispatcher.utter_message("-----" + response)
+        #dispatcher.utter_message("-----" + response)
         #return [SlotSet('location', loc)]
